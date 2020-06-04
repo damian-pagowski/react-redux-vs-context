3eee54a9c1f88265e35af379f74cd77d77d2b060
Damian Pagowski <d.pagowski@gmail.com>
2020-06-04 21:07:52 +0700

    redux to context api v2

--- src/App.js
+++ src/App.js
@@ -4,80 +4,18 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ProductsPage from "./pages/Products";
 import CartPage from "./pages/Cart";
 import "./App.css";
-import ShopContext from "./context/shop-context";
-
+import GlobalState from "./context/GlobalState";
 class App extends Component {
-  state = {
-    products: [
-      { id: "p1", title: "Gaming Mouse", price: 29.99 },
-      { id: "p2", title: "Harry Potter 3", price: 9.99 },
-      { id: "p3", title: "Used plastic bottle", price: 0.99 },
-      { id: "p4", title: "Half-dried plant", price: 2.99 },
-    ],
-    cart: [],
-    cartSum: 0,
-  };
-
-  addProductToCart = (product) => {
-    console.log("adding product : " + JSON.stringify(product));
-
-    const updatedCart = [...this.state.cart];
-    const updatedItemIndex = updatedCart.findIndex(
-      (item) => item.id === product.id
-    );
-
-    if (updatedItemIndex < 0) {
-      updatedCart.push({ ...product, quantity: 1 });
-    } else {
-      const updatedItem = {
-        ...updatedCart[updatedItemIndex],
-      };
-      updatedItem.quantity++;
-      updatedCart[updatedItemIndex] = updatedItem;
-    }
-    setTimeout(() => this.setState({ cart: updatedCart }), 700);
-  };
-
-  removeProductFromCart = (productId) => {
-    console.log("removing product: " + productId);
-
-    const updatedCart = [...this.state.cart];
-    const updatedItemIndex = updatedCart.findIndex(
-      (item) => item.id === productId
-    );
-
-    const updatedItem = {
-      ...updatedCart[updatedItemIndex],
-    };
-    updatedItem.quantity--;
-    if (updatedItem.quantity <= 0) {
-      updatedCart.splice(updatedItemIndex, 1);
-    } else {
-      updatedCart[updatedItemIndex] = updatedItem;
-    }
-    // this.setState({ cart: updatedCart });
-    setTimeout(() => this.setState({ cart: updatedCart }), 700);
-
-  };
-
   render() {
     return (
-      <ShopContext.Provider
-        value={{
-          products: this.state.products,
-          cart: this.state.cart,
-          cartSum: this.state.cartSum,
-          addProductToCart: this.addProductToCart,
-          removeProductFromCart: this.removeProductFromCart,
-        }}
-      >
+      <GlobalState>
         <BrowserRouter>
           <Switch>
             <Route path="/" component={ProductsPage} exact />
             <Route path="/cart" component={CartPage} exact />
           </Switch>
         </BrowserRouter>
-      </ShopContext.Provider>
+      </GlobalState>
     );
   }
 }
